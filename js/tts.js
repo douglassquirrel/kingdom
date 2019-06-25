5f0c08e3af06eb2e09bacdc705cff2a0cdb7710d
Douglas Squirrel <ds@douglassquirrel.com>
2019-06-25 23:07:36 +0100

    fix odd exponential reading

--- js/tts.js
+++ js/tts.js
@@ -3,14 +3,24 @@ function make_tts_engine()
     "use strict";
    
     var synth;
+    var expansions = 
+        {"R":        " rook ",
+         "N":        " knight ",
+         "B":        " bishop ",
+         "Q":        " queen ",
+         "K":        " king ",
+         "x":        " takes ",
+         "+":        " check ",
+         "#":        " checkmate ",
+         "O-O-O":    " castles queenside ",
+         "O-O":      " castles kingside ",
+         "e.p.":     " ahn pahssehhnt ",
+         "=":        " promoting to ",
+        };
+
 
     if ('speechSynthesis' in window) {
         synth = window.speechSynthesis;
-    }
-
-    function say_move(san) {
-        var move = expand_move(san);
-        say(move);
     }
 
     function say(text) {
@@ -26,30 +36,33 @@ function make_tts_engine()
         synth.speak(utterance);
     }
 
+    function say_move(san) {
+        var move = san;
+        move = fix_odd_exponential(move);        
+        move = expand_move(move);
+        say(move);
+    }
+
     function expand_move(san) {
         var move = san;
-        var expansions = 
-            {"R":        " rook ",
-             "N":        " knight ",
-             "B":        " bishop ",
-             "Q":        " queen ",
-             "K":        " king ",
-             "x":        " takes ",
-             "+":        " check ",
-             "#":        " checkmate ",
-             "O-O-O":    " castles queenside ",
-             "O-O":      " castles kingside ",
-             "e.p.":     " ahn pahssehhnt ",
-             "=":        " promoting to ",
-            };
 
         var reString = Object.keys(expansions).join("|");
         reString = reString.replace(/\+/g, "\\+");
         reString = reString.replace(/\./g, "\\.");
-        var re = new RegExp(reString,"g");
+        var re = new RegExp(reString, "g");
         move = move.replace(re, function(matched) {
             return expansions[matched];
         });
+
+        return move;
+    }
+
+    // Fix moves like "R7e2" which come out as exponentials: "R 7 times 10^2"
+    function fix_odd_exponential(move) {
+        var re2 = /^(.\d)(.*)/;
+        var match = re2.exec(move);
+        if (match) { move = match[1] + " " + match[2]; }
+
         return move;
     }        
 
